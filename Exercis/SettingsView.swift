3bd7e394ac58b74b9725c0ae40cc068f827f435f
Ruben Luthman <rubenluthman@gmail.com>
2026-06-02 22:53:22 +0200

    Fix: restore missing ShareSheet to SettingsView

--- Exercis/SettingsView.swift
+++ Exercis/SettingsView.swift
@@ -277,3 +277,11 @@ struct SettingsView: View {
         return url
     }
 }
+
+struct ShareSheet: UIViewControllerRepresentable {
+    let items: [Any]
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        UIActivityViewController(activityItems: items, applicationActivities: nil)
+    }
+    func updateUIViewController(_ vc: UIActivityViewController, context: Context) {}
+}
